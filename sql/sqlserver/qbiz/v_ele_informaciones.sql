be1f2264e59b5ad607d85b4cf01bbce0023b14aa
Jorge Luis <jorgequiguango@icloud.com>
2018-11-15 21:10:29 -0500

    Vista unpivot para v_ele_informaciones

--- sql/sqlserver/qbiz/v_ele_informaciones.sql
+++ sql/sqlserver/qbiz/v_ele_informaciones.sql
@@ -1,6 +1,7 @@
 USE [quijotelui]
 GO
 
+/****** Object:  View [dbo].[v_ele_informaciones]    Script Date: 15/11/2018 21:01:12 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -8,24 +9,38 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE VIEW [dbo].[v_ele_informaciones]
+create VIEW [dbo].[v_ele_informaciones]
 AS
-
 with informaciones as (
-select
-CodLegal as documento, 'Email' as nombre, rtrim(ltrim(lower(email))) as valor from BDQualityV.dbo.Entidad
-where email like '%@%'
-union all
-select
-CodLegal as documento, 'DirecciÃ³n' as nombre, rtrim(ltrim(Direccion)) as valor from BDQualityV.dbo.Entidad
-union all
-select
-CodLegal as documento, 'TelÃ©fono' as nombre, rtrim(ltrim(Fono)) as valor from BDQualityV.dbo.Entidad
-where Fono not like '%[^0-9]%'
-and Fono <> ''
-)
-select ROW_NUMBER() OVER(ORDER BY documento ASC) as id, documento, nombre, valor from informaciones
+SELECT ROW_NUMBER() OVER(ORDER BY entidad ASC) as id, entidad as documento, 
+replace(replace(replace(nombre, '$', ''), '!', ''), '=', '') as nombre, 
+replace(replace(replace(valor, '$', ''), '!', ''), '=', '') as valor
+FROM
+(
+  SELECT entidad,  
+  CONVERT(varchar(100), direccion+'$') as p1,
+  CONVERT(varchar(100), fono+'=') as p2,
+  CONVERT(varchar(100), lower(email)+'!') as p3,
+  CONVERT(varchar(10), 'Dirección'+'$') as n1,
+  CONVERT(varchar(10), 'Teléfono'+'=') as n2,
+  CONVERT(varchar(10), 'Email'+'!') as n3
+  FROM BDQualityv.dbo.Entidad
+  where Empresa = 'Activefun'
+  and TipoEntidad = 'CLIENTE'
+) AS cp
+UNPIVOT 
+(
+  valor FOR valores IN ( p1, p2, p3)  
+) AS up
+UNPIVOT 
+(
+  nombre FOR nombres IN ( n1, n2, n3)  
+) AS up
+where RIGHT(nombre,1) = RIGHT(valor,1)
+) select id, documento, nombre, valor from informaciones
+where valor <> ''
 
 
 GO
+
 
